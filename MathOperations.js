a220be4b9a632a020d9a15277e2ceecce24ac1bf
Sumit-9669 <sumitp9669@gmail.com>
2025-09-23 15:19:29 +0530

    Initial commit for JavaScript repository

---
+++ MathOperations.js
@@ -0,0 +1,9 @@
+let num1 = 10, num2 =20;
+let sum = num1 + num2;
+let diff = num2 - num1;
+let prod = num1 * num2;
+let div = num2 / num1;
+console.log("The sum of two numbers is: " + sum);
+console.log("The difference between two numbers is: " + diff);
+console.log("The product of two numbers is: " + prod);
+console.log("The division of two numbers is: " + div);
